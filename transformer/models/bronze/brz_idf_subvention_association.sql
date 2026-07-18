e05eb5b49630efb1c3019390fc030b60f200a66b
VictorMeyer77 <=>
2026-07-18 18:45:59 +0200

    add idf subvention association

---
+++ transformer/models/bronze/brz_idf_subvention_association.sql
@@ -0,0 +1,6 @@
+SELECT
+    *,
+    filename AS source_file_path,
+    CURRENT_TIMESTAMP AS dt_ingested,
+    REGEXP_EXTRACT(source_file_path, '_(\d{4})', 1) AS release_year
+FROM {{ source("raw", "idf_subvention_association") }}
