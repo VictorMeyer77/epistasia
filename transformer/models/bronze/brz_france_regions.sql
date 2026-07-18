c2f3e1a0a4dc33f4ec3124707577ad9b9b91e4e4
VictorMeyer77 <=>
2026-07-18 14:05:52 +0200

    bronze: enforce release year regex

--- transformer/models/bronze/brz_france_regions.sql
+++ transformer/models/bronze/brz_france_regions.sql
@@ -2,5 +2,5 @@ SELECT
     *,
     filename AS source_file_path,
     CURRENT_TIMESTAMP AS dt_ingested,
-    REGEXP_EXTRACT(source_file_path, '(\d{4})', 1) AS release_year
+    REGEXP_EXTRACT(source_file_path, '_(\d{4})', 1) AS release_year
 FROM {{ source("raw", "france_regions") }}
