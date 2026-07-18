fa91ec989e504741fd9c4110c2ce4a542eeba63d
VictorMeyer77 <=>
2026-07-18 18:23:27 +0200

    add caisse depots subvention

---
+++ transformer/models/silver/subvention/svr_subvention_caisse_depots.sql
@@ -0,0 +1,31 @@
+{{
+    config(
+        materialized="external",
+        location="../datalake/silver/subvention/subvention_caisse_depots.parquet",
+        format="parquet"
+    )
+}}
+
+SELECT
+    nomattribuant AS nom_attribuant,
+    idattribuant::TEXT AS id_attribuant,
+    dateconvention AS date_convention,
+    referencedecision AS reference_decision,
+    idbeneficiaire::TEXT AS id_beneficiaire,
+    nombeneficiaire AS nom_beneficiaire,
+    objet,
+    montant,
+    nature,
+    conditionsversement AS conditions_versement,
+    datesversement_debut AS dates_versement_debut,
+    datesversement_fin AS dates_versement_fin,
+    idrae::TEXT AS id_rae,
+    notificationue AS notification_ue,
+    pourcentagesubvention::DOUBLE AS pourcentage_subvention,
+    release_year
+FROM {{ ref("brz_caisse_depots_subvention") }}
+WHERE
+    release_year = (
+        SELECT MAX(subvention_max.release_year)
+        FROM {{ ref("brz_caisse_depots_subvention") }} AS subvention_max
+    )
